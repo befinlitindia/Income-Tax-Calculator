f53e08fcc30e6da0a841ed128e48357e33d2bfb5
befinlitindia <befinlitindia@gmail.com>
2026-01-24 17:41:34 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,10 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      {/* THIS LINE IS THE FIX: We added the basename property */}
+      <BrowserRouter basename="/Income-Tax-Calculator">
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
